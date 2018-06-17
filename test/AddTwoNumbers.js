e7af82ad58e89aa4c108da8807f68846a18a814f
junwooji <junwoo@finda.co.kr>
2018-06-17 23:46:25 +0900

    add AddTwoNumbers.js

--- test/AddTwoNumbers.js
+++ test/AddTwoNumbers.js
@@ -10,22 +10,29 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function (l1, l2) {
-    var i = 0, j = 0;
-    var num1 = 0, num2 = 0;
-    while (l1.next !== null) {
-        // var num1 += l1.val * Math.pow(10, i++);
+var addTwoNumbers = function(l1, l2) {
+    var dummyHead = new ListNode(0);
+    var carry = 0, p = l1, q = l2, cur = dummyHead;
+    while (p !== null || q !== null) {
+        var pval = p !== null ? p.val : 0;
+        var qval = q !== null ? q.val : 0;
+        var sum = pval + qval + carry;
+        cur.next = new ListNode(sum % 10);
+        carry = Math.floor(sum / 10);
+        p = p && p.next;
+        q = q && q.next;
+        cur = cur.next;
     }
-
-    while (l2.next !== null) {
-        // var num2 += (l2.val * Math.pow(10, j++));
-    }
-
-    return num1 + num2;
-};
-
+    if (carry > 0) cur.next = new ListNode(carry);
+    return dummyHead.next;
+}
 
 function ListNode(val) {
     this.val = val;
     this.next = null;
 }
+
+var a = new ListNode(1);
+a.next = new ListNode(8);
+var b = new ListNode(0);
+console.log(addTwoNumbers(a, b));
