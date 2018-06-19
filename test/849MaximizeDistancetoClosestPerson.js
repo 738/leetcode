66d03e7b35467eb0a17523d3da1edf9419a4b1c2
지준우 <junwoo@finda.co.kr>
2018-06-19 22:13:43 +0900

    solved 849, 852 completely

--- test/849MaximizeDistancetoClosestPerson.js
+++ test/849MaximizeDistancetoClosestPerson.js
@@ -2,18 +2,13 @@
  * @param {number[]} seats
  * @return {number}
  */
-var maxDistToClosest = function(seats) {
-    var max = 0;
-    var maxi = -1;
-    seats = seats.join('').split('1');
-    seats.forEach((v, i) => {
-        if (v.length > max) {
-            max = v.length;
-            maxi = i;
-        }
+let maxDistToClosest = seats => {
+    let max = 0;
+    seats.join('').split('1').forEach((v, i, a) => {
+        let tmp = (i === 0 || i === a.length - 1) ? v.length : Math.ceil(v.length/2);
+        if (tmp >= max) max = tmp;
     });
-    // if (max === seats[seats.length - 1].length) 
-    return (maxi === 0 || maxi === seats.length - 1) ? max : Math.ceil(max/2);
+    return max;
 };
 
 console.log(maxDistToClosest([1,0,0,0,1,0,1])); // 2
@@ -21,6 +16,8 @@ console.log(maxDistToClosest([1,0,0,0])); //3
 console.log(maxDistToClosest([0,1,0,0,0,0]));   // 4
 console.log(maxDistToClosest([0,1,0,0,0,1,1,0,1,1])); // 2
 console.log(maxDistToClosest([0,1,1,1,0,0,1,0,0])); // 2
+console.log(maxDistToClosest([0,0,0,1,1,0,1,1,0,0,0,1,0,0,1,0,0])) // 3
+console.log(maxDistToClosest([0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,1])) // 3
 
 // 1 -> 1
 // 2 -> 1
@@ -30,3 +27,9 @@ console.log(maxDistToClosest([0,1,1,1,0,0,1,0,0])); // 2
 // 6 -> 3
 // 7 -> 4
 // 8 -> 4
+
+// 1 -> 1
+// 2 -> 2
+// 3 -> 3
+// 4 -> 4
+// 5 -> 5
--- test/852PeakIndexinaMountainArray.js
+++ test/852PeakIndexinaMountainArray.js
@@ -2,16 +2,18 @@
  * @param {number[]} A
  * @return {number}
  */
-let peakIndexInMountainArray = A => {
-    let peak = -1, answer = -1;
-    A.forEach((v, i) => {
-        if (v > peak) {
-            peak = v;
-            answer = i;
-        }
-    });
-    return answer;
-};
+// let peakIndexInMountainArray = A => {
+//     let peak = -1, answer = -1;
+//     A.forEach((v, i) => {
+//         if (v > peak) {
+//             peak = v;
+//             answer = i;
+//         }
+//     });
+//     return answer;
+// };
+
+let peakIndexInMountainArray = A => A.indexOf(Math.max(...A));
 
 console.log(peakIndexInMountainArray([0,1,0])); // 1
 console.log(peakIndexInMountainArray([0,2,1,0])); // 1
