c7121fa23074aed5ebfbc9f516ff15569ad130ea
junwooji <junwoo@finda.co.kr>
2018-07-07 22:08:36 +0900

    solve 669

--- py/783. Minimum Distance Between BST Nodes.py
+++ py/783. Minimum Distance Between BST Nodes.py
@@ -18,3 +18,17 @@ class Solution:
         for num1, num2 in zip(nums, nums[1:]):
             res.append(num2 - num1)
         return min(res)
+
+# by recursive
+# class Solution(object):
+#     pre = -float('inf')
+#     res = float('inf')
+
+#     def minDiffInBST(self, root):
+#         if root.left:
+#             self.minDiffInBST(root.left)
+#         self.res = min(self.res, root.val - self.pre)
+#         self.pre = root.val
+#         if root.right:
+#             self.minDiffInBST(root.right)
+#         return self.res
